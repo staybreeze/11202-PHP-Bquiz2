c56dabab57b69185d7d2a489dd70ee44eb08e9b7
staybreeze <wunshengliao@gmail.com>
2024-01-05 14:27:56 +0800

    docs:新增註解

--- api/add_que.php
+++ api/add_que.php
@@ -6,18 +6,20 @@ if (isset($_POST['subject'])) {
         'subject_id' => 0,
         'vote' => 0
     ]);
+    // 方法一
     // 找到符合選項，並把他的['id']賦值給$subject_id
     $subject_id = $Que->find(['text' => $_POST['subject']])['id'];
-    $subject_id2=$Que->max('id');
+    // 方法二
+    // $subject_id=$Que->max('id');
 }
 
 echo $subject_id;
-echo $subject_id2;
 
-if(isset($_POST['option'])){
-    foreach($_POST['option'] as $option){
-        $Que->save(['text'=>$option,'subject_id'=>$subject_id,'vote'=>0]);
+
+if (isset($_POST['option'])) {
+    foreach ($_POST['option'] as $option) {
+        $Que->save(['text' => $option, 'subject_id' => $subject_id, 'vote' => 0]);
     }
 }
 
-// to("../back.php?do=que");
+to("../back.php?do=que");
